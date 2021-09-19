b93d3b0cc0b89432371b6ff4f4160463e63f971c
jnana_cetana <jnana_cetana@DESKTOP-MNGCD2S>
2021-09-20 00:48:21 +0550

    updated changes - 2021-09-20 00:28:21

--- src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -13,56 +13,39 @@ public class StandardCalculatorTest {
         standardCalculator = new StandardCalculator();
     }
     @Test
-    @DisplayName("Test Addition of Two Doubles")
-    void testAdditionOperationForDoubles(){
-        //Arrange
-        double expectedResult = 2.5;
-        //Act
-        standardCalculator.add(1.0,1.5);
-        double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Test Substraction of Two Doubles")
-    void testSubtractionOperationForDoubles(){
-        //Arrange
-        double expectedResult = -10.5;
-        //Act
-        standardCalculator.subtract(10.0,20.5);
-        double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
-    }
-
-
-    @Test
     @DisplayName("Test Addition of Two Integers")
     void testAdditionOperation(){
-        //Arrange
-        double expectedResult = 2;
-        //Act
         standardCalculator.add(1,1);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(2, actualResult);
     }
 
     @Test
     @DisplayName("Test Substraction of Two Integers")
     void testSubtractionOperation(){
-        //Arrange
-        double expectedResult = 0;
-        //Act
         standardCalculator.subtract(1,1);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(0, actualResult);
     }
 
 
+    @Test
+    @DisplayName("Test Addition of Two Doubles")
+    void testAdditionOperationForDoubles(){
+        standardCalculator.add(1.0,1.5);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(2.5, actualResult);
+    }
 
+    @Test
+    @DisplayName("Test Substraction of Two Doubles")
+    void testSubtractionOperationForDoubles(){
+        standardCalculator.subtract(10.0,20.5);
+        double actualResult = standardCalculator.getResult();
+        Assertions.assertEquals(-10.5, actualResult);
+    }
+
+    
 
 
 }
