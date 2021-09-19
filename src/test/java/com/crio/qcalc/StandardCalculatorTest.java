98ffd47c76aadf37dd13d3479b0d57ba3860b957
jnana_cetana <jnana_cetana@DESKTOP-MNGCD2S>
2021-09-19 23:28:28 +0550

    updated changes - 2021-09-19 23:08:27

--- src/test/java/com/crio/qcalc/StandardCalculatorTest.java
+++ src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -17,55 +17,38 @@ public class StandardCalculatorTest {
     @Test
     @DisplayName("Test Addition of Two Integers")
     void testAdditionOperation(){
-        //Arrange
-        double expectedResult = 2;
-        //Act
         standardCalculator.add(1,1);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(2, actualResult);
     }
 
     @Test
     @DisplayName("Test Substraction of Two Integers")
     void testSubtractionOperation(){
-        //Arrange
-        double expectedResult = 0;
-        //Act
         standardCalculator.subtract(1,1);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(0, actualResult);
     }
     
     @Test
     @DisplayName("Test Addition of Two Doubles")
     void testAdditionOperationForDoubles(){
-        //Arrange
-        double expectedResult = 2.5;
-        //Act
         standardCalculator.add(1.0,1.5);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(2.5, actualResult);
     }
 
     @Test
     @DisplayName("Test Substraction of Two Doubles")
     void testSubtractionOperationForDoubles(){
-        //Arrange
-        double expectedResult = -10.5;
-        //Act
         standardCalculator.subtract(10.0,20.5);
         double actualResult = standardCalculator.getResult();
-        //Assert
-        Assertions.assertEquals(expectedResult, actualResult);
+        Assertions.assertEquals(-10.5, actualResult);
     }
     
     @Test
     @DisplayName("Test Addition Overflow of Two Doubles")
     void testAdditionOverflowForDoubles(){
-        //Assert
         Assertions.assertThrows(ArithmeticException.class,new Executable(){
             @Override
             public void execute() throws Throwable{
@@ -76,7 +59,6 @@ public class StandardCalculatorTest {
     @Test
     @DisplayName("Test Subtraction Overflow of Two Doubles")
     void testSubtractionOverflowForDoubles(){
-        //Assert
         Assertions.assertThrows(ArithmeticException.class,new Executable(){
             @Override
             public void execute() throws Throwable{
