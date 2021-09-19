df4d2e27c9ba99668318f660d4cb9b79bb564f9a
jnana_cetana <jnana_cetana@DESKTOP-MNGCD2S>
2021-09-19 15:39:24 +0550

    first commit

---
+++ src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -0,0 +1,14 @@
+package com.crio.qcalc;
+
+public class StandardCalculator {
+
+    public static void getVersion(){
+        System.out.println("Standard Calculator 1.0");
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/com/crio/qcalc/StandardCalculatorTest.java
@@ -0,0 +1,42 @@
+package com.crio.qcalc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StandardCalculatorTest {
+    private StandardCalculator standardCalculator;
+
+    @BeforeEach
+    void setup(){
+        standardCalculator = new StandardCalculator();
+    }
+    @Test
+    @DisplayName("Test Addition of Two Integers")
+    void testAdditionOperation(){
+        //Arrange
+        int expectedResult = 2;
+        //Act
+        standardCalculator.add(1,1);
+        int actualResult = standardCalculator.getResult();
+        //Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Substraction of Two Integers")
+    void testSubtractionOperation(){
+        //Arrange
+        int expectedResult = 0;
+        //Act
+        standardCalculator.subtract(1,1);
+        int actualResult = standardCalculator.getResult();
+        //Assert
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    
+
+
+}
