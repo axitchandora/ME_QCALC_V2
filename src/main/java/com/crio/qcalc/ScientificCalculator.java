e216e463ac415a8b2a5bf1bc2fd140c30b6802c3
jnana_cetana <jnana_cetana@DESKTOP-MNGCD2S>
2021-09-20 08:50:34 +0550

    updated changes - 2021-09-20 08:30:34

--- src/main/java/com/crio/qcalc/ScientificCalculator.java
+++ src/main/java/com/crio/qcalc/ScientificCalculator.java
@@ -3,14 +3,14 @@ package com.crio.qcalc;
 public class ScientificCalculator extends StandardCalculator{
 
     public static void getVersion(){
-        System.out.println("Scientific Calculator 1.0");
+        System.out.println("Scientific Calculator 1.0";
     }
 
     public void sin(double a){
-        setResult(Math.sin(a));
+        result = Math.sin(a);
     }
     public void cos(double a){
-        setResult(Math.cos(a));
+        result = Math.cos(a);
     }
     public void square(double a){
         multiply(a,a);
