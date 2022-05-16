92e712adee25e54e5eb3190b440e1aa74236a3de
CrioUser <crio-user@criodo.com>
2022-05-17 00:40:12 +0530

    Initial commit

---
+++ src/main/java/com/crio/qcalc/QcalcApplication.java
@@ -0,0 +1,13 @@
+package com.crio.qcalc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(QcalcApplication.class, args);		
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/crio/qcalc/QcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.qcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
